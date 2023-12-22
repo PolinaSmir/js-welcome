0266b1fbe1598402c317a99ef95ec82a258ac4a3
Polina <smivasilisa@gmail.com>
2023-12-22 20:58:41 +0200

    tasks practice

--- index.js
+++ index.js
@@ -1,8 +1,49 @@
-let r = 10;
-if (r > 12) {
-  console.log("Bolshe 12");
-} else if (r < 5) {
-  console.log("Menshe 5");
+let h = 60;
+/*
+0-15 - 1
+15-30 - 2
+30-45 - 3
+45-60 - 4 peredel
+*/
+if (h >= 0 && h <= 15) {
+  console.log("1 chvert");
+} else if (h > 15 && h <= 30) {
+  console.log("2 chvert");
+} else if (h > 30 && h <= 45) {
+  console.log("3 chvert");
+} else if (h > 45 && h <= 60) {
+  console.log("4 chvert");
 } else {
-  console.log("V diapazone miz 5 i 12");
+  console.log("Error");
+}
+
+/*
+rik
+kratn 4 i ne kratn 100 ale kratn 400
+ */
+let r = 2012;
+if ((r % 4 === 0 && r % 100 !== 0) || r % 400 === 0) {
+  console.log("Vysokosny rok");
+} else {
+  console.log("Nevysokosny rok");
+}
+
+/* Task 3 */
+let number = 10;
+const start = 5;
+const end = 9;
+if (number < start || number > end) {
+  console.log("Poza diapazonom");
+} else {
+  console.log("V diapazoni");
+}
+
+/* Task 4 */
+let lidi = 10;
+let ruchka = 15;
+let blocknot = 10;
+if (ruchka >= lidi && blocknot >= lidi) {
+  console.log("Mozet");
+} else {
+  console.log("Ne mozet");
 }
