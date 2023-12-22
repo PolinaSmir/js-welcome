93e0e5c79eae666a3c43a775a3a147e41142530f
Polina <smivasilisa@gmail.com>
2023-12-22 17:28:48 +0200

    pramougolnik practice

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-// Increment, decrement
-
-let superNumber = 1;
-
-//Task: Increase superNumber by 1
-
-superNumber++;
-//console.log(superNumber); //2
-//console.log(superNumber++); //2 same thing faster
-superNumber--;
-// Prefix
-//++superNumber; --> first Increment then everything(all other operations) else
-//--superNumber; --> first decrement then everything else
-//Postfix
-//superNumber++; --> first variable, after operation and then increment
-//superNumber--; --> first variable, after operation and then decrement
-console.log(--superNumber);
+const length = 15;
+const height = 5;
+let area = length * height;
+console.log(area);
