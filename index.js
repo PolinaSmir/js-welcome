eb6b9a36eb0549a2c3ebdb5d961ede52e3a93590
Polina <smivasilisa@gmail.com>
2023-12-22 14:56:11 +0100

    const function

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-let box;
-box = 3;
-// console.log(box);
-let box2 = "Hello!";
-// console.log(box2);
-box2 = 236;
-// console.log(box2);
-let isActive = true;
-let isLogged = false;
-let x = 10;
-let y = x;
-// console.log(y);
+let box1 = "Hello";
+let box2;
+box2 = 2;
+const secretWord = "superSecret!";
+console.log(secretWord);
