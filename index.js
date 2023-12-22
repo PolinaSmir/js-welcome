ddba752332d3e423d0c2422cb4c8b4e7f0b82b7c
Polina <smivasilisa@gmail.com>
2023-12-22 16:52:15 +0200

    practise_1

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-let box1 = "Hello";
-let box2;
-box2 = 2;
-const secretWord = "superSecret!";
-console.log(secretWord);
+const box = null;
+console.log(box);
+let box1 = 5;
+console.log(box1);
+let box2 = "2";
+console.log(box2);
+let result = box1 + box2;
+console.log(result);
+result = box2 - box1;
+console.log(result);
+result = box + box1;
+console.log(result);
