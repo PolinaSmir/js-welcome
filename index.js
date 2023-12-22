c1564b7a3b74f029babefe227dc151a4a145ba83
Polina <smivasilisa@gmail.com>
2023-12-22 14:51:50 +0100

    let use

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-// () call function
-console.log(100);
-
-// alert("Hello World!");
-
-console.log("test");
-
-console.log(true);
-console.log(null);
-console.log(5n);
+let box;
+box = 3;
+// console.log(box);
+let box2 = "Hello!";
+// console.log(box2);
+box2 = 236;
+// console.log(box2);
+let isActive = true;
+let isLogged = false;
+let x = 10;
+let y = x;
+console.log(y);
