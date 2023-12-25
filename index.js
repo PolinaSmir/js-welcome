c919571589e95f6db6b37ee075482eeb287a2064
Polina <smivasilisa@gmail.com>
2023-12-25 19:51:52 +0200

    Function Welcome

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-let firstOperand = Number(prompt("Put in first number: "));
-let secondOperand = Number(prompt("Put in second number: "));
-let result = firstOperand + secondOperand;
-if (Number.isNaN(result)) {
-  console.log("Ты не прав, введи число");
-} else {
-  console.log(result);
+function greetingUser(userName) {
+  console.log("Hello, " + userName);
 }
+greetingUser("Alex");
+greetingUser("John");
+greetingUser("Sam");
+greetingUser("Solar");
