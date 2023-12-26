3d129c91219b5e89b2b2b17cbc02202357e015d5
Polina <smivasilisa@gmail.com>
2023-12-26 18:10:13 +0200

    My variant of hw-fix hw later

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-greetingUser("James");
+// function biggerNumber(num1, num2) {
+//   if (num1 > num2) {
+//     console.log(num1);
+//   } else if (num1 === num2) {            // Peredel
+//     console.log(num1);
+//   } else {
+//     console.log(num2);
+//   }
+// }
 
-function greetingUser(userName) {
-  console.log("Hi, " + userName);
+// biggerNumber(4, 5);
+
+function averageOfTwoNumbers(num1, num2) {
+  const result = (num1 + num2) / 2;
+  return result;
 }
 
-greetingUser("Alex");
-// other types of functions on the other git
+// console.log(averageOfTwoNumbers(1, 2));
+
+const number = Number(prompt("Enter number: "));
+//const number2 = Number(prompt('Enter number2: '));
+
+function evenNumber(number) {
+  if (number % 2 === 0) {
+    console.log(number + " является чётным числом");
+  } else {
+    console.log(number + " не является чётным числом");
+  }
+}
+evenNumber(number);
