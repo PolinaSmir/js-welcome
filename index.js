e7a53c1349bb9981c08e00d5d630c0f4aeb7c38e
Polina <smivasilisa@gmail.com>
2023-12-26 20:30:56 +0200

    switch case default start-example

--- index.js
+++ index.js
@@ -1,11 +1,43 @@
-// Task 1
-const isAuth = false;
-const viewMode = isAuth ? "authMode" : "unauth";
-console.log(viewMode);
+// const number = Number(prompt("Введите число: "));
 
-// Task 2
+// if (number % 5 === 0) {
+//   console.log(number + " нацело делиться на 5");
+// } else if (number % 3 === 0) {
+//   console.log(number + " нацело делиться на 3");
+// } else if (number % 2 === 0) {
+//   console.log(number + " нацело делиться на 2");
+// } else {
+//   console.log(number + " не делиться нацело ни на 5, ни на 3, ни на 2");
+// }
 
-const number = 13;
-//diap od 6 do 12
-const inOrOutDiap = number < 6 || number > 12 ? "out" : "in";
-console.log(inOrOutDiap);
+const monthNumber = 12;
+
+switch (monthNumber) {
+  case 1:
+  case 2:
+  case 12: {
+    console.log("zima");
+    break;
+  }
+  case 3:
+  case 4:
+  case 5: {
+    console.log("jaro");
+    break;
+  }
+  case 6:
+  case 7:
+  case 8: {
+    console.log("leto");
+    break;
+  }
+  case 9:
+  case 10:
+  case 11: {
+    console.log("podzim");
+    break;
+  }
+  default: {
+    console.log("Enter number from 1 to 12");
+  }
+}
