8d0b99a526916404c1c8885ff220b4707ab1ed67
Polina <smivasilisa@gmail.com>
2023-12-26 19:58:02 +0200

    Ternarniy operator task1, 2

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// Task 1
 const isAuth = false;
 const viewMode = isAuth ? "authMode" : "unauth";
 console.log(viewMode);
+
+// Task 2
+
+const number = 13;
+//diap od 6 do 12
+const inOrOutDiap = number < 6 || number > 12 ? "out" : "in";
+console.log(inOrOutDiap);
