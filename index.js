bc8de2586a38ef40021df1b00aede1567b4e2a61
Polina <smivasilisa@gmail.com>
2023-12-26 19:50:30 +0200

    practice const with ?

--- index.js
+++ index.js
@@ -1,30 +1,3 @@
-// function biggerNumber(num1, num2) {
-//   if (num1 > num2) {
-//     console.log(num1);
-//   } else if (num1 === num2) {            // Peredel
-//     console.log(num1);
-//   } else {
-//     console.log(num2);
-//   }
-// }
-
-// biggerNumber(4, 5);
-
-function averageOfTwoNumbers(num1, num2) {
-  const result = (num1 + num2) / 2;
-  return result;
-}
-
-// console.log(averageOfTwoNumbers(1, 2));
-
-const number = Number(prompt("Enter number: "));
-//const number2 = Number(prompt('Enter number2: '));
-
-function evenNumber(number) {
-  if (number % 2 === 0) {
-    console.log(number + " является чётным числом");
-  } else {
-    console.log(number + " не является чётным числом");
-  }
-}
-evenNumber(number);
+const isAuth = false;
+const viewMode = isAuth ? "authMode" : "unauth";
+console.log(viewMode);
