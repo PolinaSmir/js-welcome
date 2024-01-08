baf4035cecf6bec47ad42da51ec66aebc2a07958
Polina <smivasilisa@gmail.com>
2024-01-08 19:46:49 +0100

    Practice about salary

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
-// function greeting(user) {
-//   return `Hello ${"name" in user ? user.name : "Anonym"}`;
-// }
-
-// const user = {
-//   name: "Peter",
-//   email: "john@email.com",
-// };
-
-const user = {
-  name: "John",
-  lastName: "Doe",
-  age: 20,
-  "favourite color": "red",
-  movie: "Twilight",
-  avatar: "http://.....",
-};
-// for (let key in user) {
-//   console.log(`${key} ---> ${user[key]}`); // key ---> value
-// }
-
-const obj = {
-  key1: "value 1",
-  key2: "valUe 2",
-  key3: "valuE 3",
+const departmentSalary = {
+  HR: 120000,
+  development: 550000,
+  PR: 50000,
+  marketing: 120000,
+  assistant: undefined,
+  ab: "gsf",
 };
 
-function style(obj) {
-  for (let key in obj) {
-    console.log(`"${key}": *${obj[key]}*`);
+function sumOfWasteSalary(departmentSalary) {
+  let sum = 0;
+  let nameOfUndefined;
+  for (let key in departmentSalary) {
+    if (typeof departmentSalary[key] === "number") {
+      sum += departmentSalary[key];
+    } else {
+      nameOfUndefined = key;
+      console.log(
+        `You have undefined salary for department: ${nameOfUndefined}`
+      );
+    }
   }
+  return sum;
 }
+
+console.log(sumOfWasteSalary(departmentSalary));
