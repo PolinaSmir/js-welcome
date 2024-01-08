0c6967ae336409b8585a1f6bbb5a709ad44cd588
Polina <smivasilisa@gmail.com>
2024-01-08 18:33:00 +0100

    Function + for in

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
-function greeting(user) {
-  return `Hello ${"name" in user ? user.name : "Anonym"}`;
-}
+// function greeting(user) {
+//   return `Hello ${"name" in user ? user.name : "Anonym"}`;
+// }
+
+// const user = {
+//   name: "Peter",
+//   email: "john@email.com",
+// };
 
 const user = {
-  name: "Peter",
-  email: "john@email.com",
+  name: "John",
+  lastName: "Doe",
+  age: 20,
+  "favourite color": "red",
+  movie: "Twilight",
+  avatar: "http://.....",
 };
+// for (let key in user) {
+//   console.log(`${key} ---> ${user[key]}`); // key ---> value
+// }
+
+const obj = {
+  key1: "value 1",
+  key2: "valUe 2",
+  key3: "valuE 3",
+};
+
+function style(obj) {
+  for (let key in obj) {
+    console.log(`"${key}": *${obj[key]}*`);
+  }
+}
