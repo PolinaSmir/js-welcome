afb1af28354476e2764a222ea8e7d287d71e596f
Polina <smivasilisa@gmail.com>
2024-01-08 18:04:42 +0100

    in

--- index.js
+++ index.js
@@ -1,34 +1,3 @@
-// const user = {
-//   name: "John",
-//   "favourite color": "red",
-//   0: "value",
-// };
-
-// console.log(user.name);
-// // user.'favourite color'; syntax error
-// // user.0; also syntax error
-// console.log(user["favourite color"]);
-// console.log(user[0]);
-
-// const someObj = {
-//   2: 10,
-// };
-
-// console.log(someObj[2]);
-// console.log(someObj["2"]);
-
-const greets = {
-  15: "Hi",
-  17: "Yo, whats up",
-  19: "Hello",
-  30: "Good morning",
-};
-
-const valueFromUser = 19;
-
-console.log(greets[valueFromUser]); // Hello
-// greets[19]
-
 const obj = {
   Mon: "drive lesson",
   Tue: "dentist visit",
@@ -36,9 +5,7 @@ const obj = {
   Thu: "hard work",
   Fri: "chill",
   Sat: "go to park",
+  0: "a",
 };
 
-const nameOfDay = prompt(
-  "Type your week day to check your plan: \n Monday = Mon, \n Tuesday = Tue, \n ..."
-);
-alert(obj[nameOfDay] ? obj[nameOfDay] : "Error");
+console.log("name" in obj);
