000bf8f76ea4d4a348531e78cfea9f4c88653623
Polina <smivasilisa@gmail.com>
2024-01-09 18:29:39 +0100

    Zadacha 2

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const arrOfNum = [3, 6, 8, 2, 3, 5, 1];
 
 function sumOfElements(arr) {
@@ -9,3 +10,18 @@ function sumOfElements(arr) {
 }
 
 console.log(sumOfElements(arrOfNum));
+*/
+
+const arrOfNum = [3, 6, 8, 2, 3, 5, 1];
+
+function maxNumberOfArray(arr) {
+  let max = arr[0];
+  for (let i = 0; i < arr.length; i++) {
+    if (max < arr[i]) {
+      max = arr[i];
+    }
+  }
+  return max;
+}
+
+console.log(maxNumberOfArray(arrOfNum));
