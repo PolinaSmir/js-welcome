00081e3fdd1dbfd555c44c8656d84933b82865ad
Polina <smivasilisa@gmail.com>
2024-01-15 17:50:47 +0100

    sort

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-const oldArray = [2, 3, 4, 5, 1, 2];
-const newArray = oldArray
-  .map((item) => {
-    return item * 2;
-  })
-  .reverse()
-  .join(",");
+const arr = [2, 5, 14, 7, 8, 7, 2, 9, 8, 1];
+arr.sort();
+const letter = ["aaa", "ada", "aay"];
+letter.sort();
+
+function compareFunction(a, b) {
+  if (b > a) {
+    return -1;
+  } else {
+    return 1;
+  }
+}
+arr.sort(compareFunction);
+
+const numbersArray = [4, 1, 58, 10, 6, -5];
+function compareNumbers(a, b) {
+  return a - b; // reverse b-a // a-b vozrastanie
+}
+numbersArray.sort(compareNumbers);
