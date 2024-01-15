d99fd9585bf7d758b95773834f5b4d700b0c6d89
Polina <smivasilisa@gmail.com>
2024-01-15 19:46:25 +0100

    MyArray

--- index.js
+++ index.js
@@ -1,49 +1,36 @@
-// const numbers = [5, 3, -8, 21, -5, -2, 4];
-// function filterFuction(item) {
-//   return item > 0;
-// }
-// const filteredNumbersArray = numbers.filter(filterFuction);
+// function MyArray() {
+//   this.length = 0;
 
-// const array = [5, 3, -8, 21, -5, -2, 4];
-// function parniElement(item) {
-//   return item % 2 === 0; /// vozvrashaet true/false
+//   this.push = function (value) {
+//     this[this.length] = value;
+//     this.length++;
+//     return this.length;
+//   };
 // }
-// const filteredArray = array.filter(parniElement);
 
-const numbersArray = [2, 3, 1, 2, 34, 21];
-function reducer(accumulator, item) {
-  return accumulator + item;
+function MyArray() {
+  this.length = 0;
+
+  this.push = function (value) {
+    for (let i = 0; i < arguments.length; i++) {
+      this[this.length] = arguments[i];
+      this.length++;
+    }
+    return this.length;
+  };
+  this.pop = function () {
+    if (this.length > 0) {
+      const lastItem = this[this.length - 1];
+      delete this[this.length - 1];
+      this.length--;
+      return lastItem;
+    } else {
+      return undefined;
+    }
+  };
 }
-const sum = numbersArray.reduce(reducer, 0); // 0 nachaln znach, s kotorogo my nacinajem scitat sumu
-
-// sobak po age od starsh do mladsh
-const dogArray = [
-  {
-    nickname: "Tuzik",
-    color: "black",
-    age: 2,
-  },
-  {
-    nickname: "Dolly",
-    color: "white",
-    age: 3,
-  },
-  {
-    nickname: "Stus",
-    color: "black",
-    age: 5,
-  },
-  {
-    nickname: "Fagot",
-    color: "black",
-    age: 9,
-  },
-];
-
-dogArray.sort((dog1, dog2) => {
-  return dog2.age - dog1.age;
-});
-// filter and make only white dogs
-const dogWhite = dogArray.filter((dog) => {
-  return dog.color === "white";
-});
+const array = new MyArray();
+array.push(1);
+array.push(2);
+array.push(3, 1, 3, 411, 43, 11);
+array.pop();
