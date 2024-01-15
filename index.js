b14e89ca114204f53899f8651a10be6987f383e7
Polina <smivasilisa@gmail.com>
2024-01-15 17:21:04 +0100

    map ()=>

--- index.js
+++ index.js
@@ -1,43 +1,7 @@
-// const array = [1, 2, 3, 4, 5];
-
-// array.forEach((a) => {
-//   console.log(a * a);
-// });
-
-// // const square = (a) => a * a; -- strelocnaja funkce
-
-// const new_arr = array.map((item) => {
-//   return item - 1;
-// });
-// function minusOne(a) {
-//   return a - 1;
-// }
-
-const users = [
-  {
-    name: "John",
-    lastName: "Doe",
-    age: 19,
-    email: "ligma@mail.com",
-  },
-  {
-    name: "Jane",
-    lastName: "Doe",
-    age: 22,
-    email: "ligmajane@mail.com",
-  },
-  {
-    name: "Jackson",
-    lastName: "Michael",
-    age: 55,
-    email: "jackson@mail.com",
-  },
-];
-
-function newUserObject(user) {
-  return {
-    fullname: `${user.name} ${user.lastName}`,
-    email: user.email,
-  };
-}
-const new_users = users.map(newUserObject);
+const oldArray = [2, 3, 4, 5, 1, 2];
+const newArray = oldArray
+  .map((item) => {
+    return item * 2;
+  })
+  .reverse()
+  .join(",");
