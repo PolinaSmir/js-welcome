720b6bb685f08db3945a0cf85088cf6f0a5d553c
Polina <smivasilisa@gmail.com>
2024-01-15 17:54:30 +0100

    filter

--- index.js
+++ index.js
@@ -1,19 +1,5 @@
-const arr = [2, 5, 14, 7, 8, 7, 2, 9, 8, 1];
-arr.sort();
-const letter = ["aaa", "ada", "aay"];
-letter.sort();
-
-function compareFunction(a, b) {
-  if (b > a) {
-    return -1;
-  } else {
-    return 1;
-  }
+const numbers = [5, 3, -8, 21, -5, -2, 4];
+function filterFuction(item) {
+  return item > 0;
 }
-arr.sort(compareFunction);
-
-const numbersArray = [4, 1, 58, 10, 6, -5];
-function compareNumbers(a, b) {
-  return a - b; // reverse b-a // a-b vozrastanie
-}
-numbersArray.sort(compareNumbers);
+const filteredNumbersArray = numbers.filter(filterFuction);
