14ed908c9c3cea3f80cb24cfaee969b0105e5830
Polina <smivasilisa@gmail.com>
2024-01-19 17:03:00 +0100

    for i for j

--- index.js
+++ index.js
@@ -45,3 +45,20 @@ const newspaper = {
   },
 };
 newspaper.showArticles();
+
+const number = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+function sumArray(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array[i].length; j++) {
+      sum += array[i][j];
+    }
+  }
+  return sum;
+}
+console.log(sumArray(number));
