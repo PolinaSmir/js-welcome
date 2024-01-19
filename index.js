0c2f9d9f30a68ac68152d33c45a94815452b63c0
Polina <smivasilisa@gmail.com>
2024-01-19 19:15:58 +0100

    nov metody

--- index.js
+++ index.js
@@ -1,64 +1,95 @@
-"use strict";
+let stri = "hello";
+// string.charAt(index) === str[index] - vozvr symbol za index
+// more at gitHub bookmark
+console.log(stri.toUpperCase());
 
-console.log(this);
+const bool = true;
+console.log(bool.toString());
 
-function test() {
-  console.log(this);
-}
-test();
+console.log(Number.isInteger(6));
+console.log(Number.isNaN("hgdfh")); // false
+console.log(Number("hgdfh")); /// NaN
+console.log(Number.isNaN(Number("hgdfh"))); // true
 
-const test2 = function () {
-  console.log(this);
-};
+/* toFixed
+const number =15.222222
 
-test2();
+number.toFixed()
+// '15'
 
-const test3 = () => {
-  console.log(this);
-};
+number.toFixed(3)
+// '15.222'
 
-test3();
+typeof number.toFixed(3)
+// 'string'
 
-const newspaper = {
-  title: "News",
-  articles: [
-    {
-      author: "John Doe",
-      date: "23-08-2023",
-      text: "lorem",
-    },
-    {
-      author: "Richard Doe",
-      date: "23-08-2023",
-      text: "lorem",
-    },
-    {
-      author: "Sam Doe",
-      date: "23-08-2023",
-      text: "lorem",
-    },
-  ],
-  showArticles: function () {
-    this.articles.forEach((item, index) => {
-      console.log(`${this.title} ${index} - ${item.author}`);
-    });
-  },
-};
-newspaper.showArticles();
 
-const number = [
-  [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
-];
+const num =15.5555
 
-function sumArray(array) {
-  let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < array[i].length; j++) {
-      sum += array[i][j];
-    }
-  }
-  return sum;
-}
-console.log(sumArray(number));
+num.toFixed()
+// '16'
+
+*/
+/*
+
+const str1 = 'Test'
+// undefined
+
+const str2 = ' --- this is test'
+// undefined
+
+str1.concat(str2)
+// 'Test --- this is test'
+
+///////////////////////////////////////////
+
+const textStr = '     text text text         '
+//undefined
+
+textStr.trim()
+//'text text text'
+
+textStr
+//'     text text text         '
+
+////////////////////////////////////////////
+
+const str = 'hi hi hi'
+//undefined
+
+str.split(' ')
+// (3) ['hi', 'hi', 'hi']0: "hi"1: "hi"2: "hi"length: 3[[Prototype]]: Array(0)
+
+'sgagd*dshfgh*gyfds;*hftjr;tr*'.split('*')
+// (5) ['sgagd', 'dshfgh', 'gyfds;', 'hftjr;tr', '']
+
+const test = 'AAAAAAAAAAAAA'
+//undefined
+
+test.toLocaleLowerCase()
+//'aaaaaaaaaaaaa'
+
+'AAAAAAAAaaaaaaa'.toLocaleLowerCase()
+//'aaaaaaaaaaaaaaa'
+
+////////////////////////////////////////////
+
+Array.from('flower')
+//(6) ['f', 'l', 'o', 'w', 'e', 'r']
+
+Array.from('flower').reverse()
+//(6) ['r', 'e', 'w', 'o', 'l', 'f']
+
+Array.from('flower').reverse().join('')
+//'rewolf'
+
+*/
+
+let number = "09934352611";
+undefined;
+const arrayFromNumber = Array.from(number);
+arrayFromNumber.unshift("+", "3", "8");
+number = arrayFromNumber.join("");
+
+let wordString = "table";
+wordString = Array.from(wordString).reverse().join("");
