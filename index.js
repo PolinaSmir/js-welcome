d7b298e0e09683b3f1b3c380c241ab981643e69a
Polina <smivasilisa@gmail.com>
2024-01-21 11:39:24 +0100

    hw palindrom and checkSpam + forEach

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 function capitaliseWords(str) {
   let words = str.split(" ");
   for (let i = 0; i < words.length; i++) {
@@ -23,3 +24,43 @@ function checkSpam(str) {
 console.log(checkSpam("buy ViAgRa now"));
 console.log(checkSpam("free xxxxxx"));
 console.log(checkSpam("rabbit innocent"));
+
+function isPalindrom(str) {
+  if (str.toLowerCase() === Array.from(str.toLowerCase()).reverse().join("")) {
+    return `${str} - палiндром`;
+  } else {
+    return `${str} - не палiндром`;
+  }
+}
+
+console.log(isPalindrom("Anna"));
+console.log(isPalindrom("Mama"));
+console.log(isPalindrom("Namman"));
+*/
+
+function MyArray() {
+  this.length = 0;
+
+  this.push = function (value) {
+    for (let i = 0; i < arguments.length; i++) {
+      this[this.length] = arguments[i];
+      this.length++;
+    }
+    return this.length;
+  };
+
+  this.forEach = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+      callback(this[i], i, this);
+    }
+  };
+}
+
+const arr = new MyArray();
+arr.push(1);
+arr.push(2);
+arr.push(3, 2, 5, 10, 12, 33);
+const a = [1, 2, 3];
+arr.forEach((item) => {
+  console.log(item);
+});
