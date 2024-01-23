fcf6bf89697e2099390afd951d632e752a0f1a00
Polina <smivasilisa@gmail.com>
2024-01-23 18:43:33 +0100

    try catch

--- index.js
+++ index.js
@@ -1,42 +1,29 @@
-const date = new Date();
-const date1 = new Date("12-31-2023");
-
-console.log(date1.getDate());
-console.log(date1.getDay());
-console.log(date1.getFullYear());
-console.log(date.getHours());
-console.log(date1.getTime());
-
-let sum = 0;
-const time1 = new Date();
-for (let i = 0; i <= 1000000; i++) {
-  sum += i;
+/**
+ * Повертае х у ступени n
+ *
+ * @param {number} x число, яке треба пиднести до степеня
+ * @param {number} n степень, у яку треба пиднести
+ * @return {number} х пиднесене у n-нну степинь
+ */
+function pow(x, n) {
+  return x ** n;
 }
 
-const time2 = new Date();
-
-console.log(sum);
-
-console.log(time2.getTime() - time1.getTime());
-
-function factorial(x) {
-  if (x === 1) {
-    return x;
-  } else {
-    return x * factorial(x - 1);
+function sumOfPositiveNumber(a, b) {
+  if (a < 0 || b < 0) {
+    const error = new RangeError("Какое-то из чисел меньше 0");
+    throw error;
   }
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new TypeError("Параметры a и b должны быть числами");
+  }
+  return a + b;
 }
 
-console.log(factorial(5));
-
-function findFibonacci(n) {
-  if (n === 0) {
-    return 0;
-  }
-  if (n === 1) {
-    return 1;
-  }
-
-  return findFibonacci(n - 1) + findFibonacci(n - 2);
+try {
+  sumOfPositiveNumber(1, -2);
+} catch (error) {
+  console.log(error);
 }
-console.log(findFibonacci(4)); // оно раскрывается до значения 1 и 0 и потом схлопывается в число по формуле(т.к. там много функций то там формулы)
+
+console.log("Usual");
