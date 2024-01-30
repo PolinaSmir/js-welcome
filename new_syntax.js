af321fc67a8d9738dcd952202b6e64a6e6f2a901
Polina <smivasilisa@gmail.com>
2024-01-30 20:54:06 +0100

    mySolution for RangeValidator

--- new_syntax.js
+++ new_syntax.js
@@ -1,16 +1,44 @@
-class Test {
-  constructor(value1, value2) {
-    this.key1 = value1;
-    this.key2 = value2;
+class RangeValidator {
+  constructor(from, to) {
+    this.from = from;
+    this.to = to;
   }
 
-  method() {
-    // method's body
+  set from(newValue) {
+    if (typeof newValue !== "number") {
+      throw new TypeError("From must be a number");
+    }
+    if (newValue > this._to) {
+      throw new RangeError("From must be less than To");
+    }
+    this._from = newValue;
   }
-  static myStaticMethod(parameter) {
-    console.log(parameter);
+
+  set to(newValue) {
+    if (typeof newValue !== "number") {
+      throw new TypeError("To must be a number");
+    }
+    if (newValue < this._from) {
+      throw new RangeError("To must be bigger than From");
+    }
+    this._to = newValue;
+  }
+
+  get from() {
+    return this._from;
+  }
+
+  get to() {
+    return this._to;
+  }
+
+  getRange() {
+    let arr = [this._from];
+    for (let i = 0; i < this._to - this._from; i++) {
+      arr.push(arr[i] + 1);
+    }
+    return arr;
   }
 }
 
-Test.myStaticMethod("123");
-const obj1 = new Test("test1", "tast2");
+const object1 = new RangeValidator(1, 5);
