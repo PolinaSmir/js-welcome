e9a416b975dde7505fb40e44d848feb40099bc22
Polina <smivasilisa@gmail.com>
2024-02-02 21:26:25 +0100

    queue

--- index.js
+++ index.js
@@ -1,51 +1,44 @@
-const arr = [2, 4, 1, 3, 7, 3, 2, 1, 3, 5, 2, 8, 2, 4, 1];
+const user1 = {
+  firstName: "Alex",
+  lastName: "Doe",
+  id: 1,
+};
 
-function linearSearch(array, value) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] === value) {
-      return i;
-    }
+const user2 = {
+  firstName: "John",
+  lastName: "Doe",
+  id: 2,
+};
+const johnMessages = ["hello", "How are you?"];
+const alexMessages = ["hi!", "I`m fine"];
+
+const findMessage = new Map();
+
+findMessage.set(user1, user1.id);
+findMessage.set(user2, user2.id);
+
+class Queue {
+  constructor() {
+    this._head = 0;
+    this._tail = 0;
   }
-  return -1;
-}
-
-console.log(linearSearch(arr, 9));
-
-function multyTable(limit) {
-  const table = [];
-  for (let i = 1; i <= limit; i++) {
-    for (let j = 1; j <= limit; j++) {
-      table.push(`${i} * ${j} = ${i * j}`);
-    }
-  }
-  return table;
-}
-
-multyTable(1);
-multyTable(3);
-
-const arr2 = [2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-function binarySearch(array, whatToFind) {
-  //  need review!!!
-  if (whatToFind > array.length - 1) {
-    return -Infinity;
+  get size() {
+    return this._tail - this._head;
   }
 
-  let start = 0;
-  let end = array.length - 1;
-  let middle = Math.round((start + end) / 2);
-
-  while (true) {
-    if (array[middle] === whatToFind) {
-      return middle;
+  enqueue(value) {
+    this[this._tail] = value;
+    this._tail++;
+    return this.size;
+  }
+  dequeue() {
+    if (this.size > 0) {
+      const firstItem = this[this._head];
+      delete this[this._head];
+      this._head++;
+      return firstItem;
     }
-    if (array[middle] < whatToFind) {
-      start = middle;
-      middle = Math.ceil((start + end) / 2);
-    } else {
-      end = middle;
-      middle = Math.ceil((start + end) / 2);
-    }
+    return undefined;
   }
 }
+const queue = new Queue();
